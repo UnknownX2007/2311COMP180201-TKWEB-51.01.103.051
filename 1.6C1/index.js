aec3a2abfb5c07217713f3575f5dd4175dd35687
UnknownX2007 <90136117+UnknownX2007@users.noreply.github.com>
2025-10-07 09:46:09 +0700

    Add files via upload

---
+++ 1.6C1/index.js
@@ -0,0 +1,20 @@
+function getAB(){  
+
+    var a = 0;
+
+    a = document.getElementById("a").value;
+
+    var b = 0;
+
+    b = document.getElementById("b").value;
+
+    if (a == "VoDinhKhoi") {
+
+
+    if (b == "VoDinhKhoi2007!") {
+
+        document.writeln("Admin la cai gi, co an duoc khong !?");
+
+    } else alert("Sai mật khẩu!");
+
+  } else alert("Sai tên đăng nhập!");
